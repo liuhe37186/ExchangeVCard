5450153cb781851dc74abe346fbbf065f2adeb03
liuhe <liuhe37186@163.com>
2015-05-22 14:58:17 +0800

    first commit
    first commit

---
+++ app/src/main/java/com/txcap/exchangevcard/MainActivity.java
@@ -0,0 +1,66 @@
+package com.txcap.exchangevcard;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+
+
+public class MainActivity extends FragmentActivity {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    private void initView(){
+        viewPager = (ViewPager) this.findViewById(R.id.id_viewpager);
+    }
+
+/*
+    public static class PlaceholderFragment extends Fragment {
+        *//**
+         * The fragment argument representing the section number for this
+         * fragment.
+         *//*
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        *//**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         *//*
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+    }*/
+
+}
